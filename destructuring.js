5e90c5b46ecec4f8bf61e1178af8e91324ad3f15
Lakshit1031 <lakshitteotia54@gmail.com>
2025-04-25 12:25:58 +0530

    25/04/25 destructuring

---
+++ destructuring.js
@@ -0,0 +1,48 @@
+// const person = {
+//   firstname: 'John',
+//   lastname: 'Doe',
+//   age: 30
+// }
+
+// let { firstname, lastname, age, country = "India" } = person;
+
+// console.log(firstname); // John
+// console.log(lastname); // Doe
+// console.log(age); // 30
+// console.log(country); // India
+// console.log(person); // { firstname: 'John', lastname: 'Doe', age: 30, country: 'India' }
+
+//destructuring is not destructive
+
+// let fname = "JohnDoe";
+// let [a1,a2,a3,a4,a5] = fname; //undefined
+// console.log(a1); // undefined
+// console.log(a2); // undefined       
+// console.log(a3); // undefined
+// console.log(a4); // undefined
+// console.log(a5); // undefined
+
+// const fruits = [ 'apple', 'banana', 'orange', 'mango' ];
+// let [fruit1,,,fruit2] = fruits; 
+// console.log(fruit1); 
+// console.log(fruit2); 
+
+// let {[0]: fruit1, [1]: fruit2} = fruits
+
+// const numbers = [10,20,30,40,50,60,70];
+
+// const[a,b, ...rest] = numbers
+// console.log(a,b);
+
+// const fruits = new Map([
+//     ['apple', 500],
+//     ['banana', 300],
+//     ['orange', 200]
+// ]);
+
+// let text = "";
+// for(const[key,value] of fruits){
+//     text+= key+ "is" +value;
+// }
+// console.log(text)
+
--- map.js
+++ map.js
@@ -1,52 +1,76 @@
-const fruits = new Map([
-    ['apple', 500],
-    ['banana', 300],
-    ['orange', 200]
-]);
-console.log(fruits);
+// const fruits = new Map([
+//     ['apple', 500],
+//     ['banana', 300],
+//     ['orange', 200]
+// ]);
+// console.log(fruits);
 
-const fruits2 = new Map();
-fruits2.set('apple', 500);
-fruits2.set('banana', 300);
-fruits2.set('orange', 200);
-console.log(fruits2);
+// const fruits2 = new Map();
+// fruits2.set('apple', 500);
+// fruits2.set('banana', 300);
+// fruits2.set('orange', 200);
+// console.log(fruits2);
 
-console.log(fruits.get('apple')); // 500
-console.log(typeof fruits); // 300
-console.log(fruits instanceof Map); // true
+// console.log(fruits.get('apple')); // 500
+// console.log(typeof fruits); // 300
+// console.log(fruits instanceof Map); // true
 
-//JAVASCRIPT MAP METHODS
-console.log(fruits.size); // 3
-// fruits.delete('banana'); // remove banana from the map
-console.log(fruits.size); // 2
-//fruits.clear(); // remove all elements from the map
-console.log(fruits.has('banana')); // false
-// fruits.forEach((value, key) => {
-//     console.log(key, value); // apple 500 orange 200
+// //JAVASCRIPT MAP METHODS
+// console.log(fruits.size); // 3
+// // fruits.delete('banana'); // remove banana from the map
+// console.log(fruits.size); // 2
+// //fruits.clear(); // remove all elements from the map
+// console.log(fruits.has('banana')); // false
+// // fruits.forEach((value, key) => {
+// //     console.log(key, value); // apple 500 orange 200
+// // });
+
+// let text = "";
+// fruits.forEach(function(value, key) {
+//     text += key + " = " + value + "\n";
 // });
+// console.log(text); // apple = 500 orange = 200
+// console.log(typeof text); // string
+// console.log(text instanceof String); // false
 
-let text = "";
-fruits.forEach(function(value, key) {
-    text += key + " = " + value + "\n";
-});
-console.log(text); // apple = 500 orange = 200
-console.log(typeof text); // string
-console.log(text instanceof String); // false
+// let textEntries = "";
+// for (const x of fruits.entries()){
+//     textEntries += x;
+// }
+// console.log(textEntries); // [object Map Iterator] [object Map Iterator]
 
-let textEntries = "";
-for (const x of fruits.entries()){
-    textEntries += x;
-}
-console.log(textEntries); // [object Map Iterator] [object Map Iterator]
+// let textKeys = "";
+// for (const x of fruits.keys()){
+//     textKeys += x;
+// }
+// console.log(textKeys); // appleorange
 
-let textKeys = "";
-for (const x of fruits.keys()){
-    textKeys += x;
-}
-console.log(textKeys); // appleorange
+// let textValues = "";
+// for (const x of fruits.values()){
+//     textValues += x;
+// }
+// console.log(textValues); 
 
-let textValues = "";
-for (const x of fruits.values()){
-    textValues += x;
-}
-console.log(textValues); 
+
+// const apples = {name: 'apples'}
+// const bananas = {name: 'bananas'}
+// const oranges = {name: 'oranges'}
+// const fruits = new Map();
+// fruits.set(apples, 500);
+// fruits.set(bananas, 200);
+
+// const fruits = [
+//     {name: 'apples', quantity: 500},
+//     {name: 'bananas', quantity: 200},
+//     {name: 'oranges', quantity: 700},
+//     {name: 'grapes', quantity: 1000}
+// ];
+
+// function myFunction({quantity}) {
+//     return quantity > 200 ? 'Ok' : 'Not Ok';
+// }
+
+// const result = Map.groupBy(fruits, myFunction);
+// console.log(result);
+
+
